89a75125b0c8cef3d3765b52d426c82997ada3b5
YK. Byeon <ykbyeon@gmail.com>
2024-10-08 18:19:04 +0900

    promise 테스트 샘플 코드

--- CodeFactory/test.js
+++ CodeFactory/test.js
@@ -68,18 +68,56 @@
 
 const getPromise = (seconds) => new Promise((resolve, reject) => {
     setTimeout(() => {
-        //resolve('완료');
-        reject('에러');
+        if (seconds % 2 === 0) {
+            resolve('완료');    // 짝수 초
+        } else {
+            reject('에러');     // 홀수 초
+        }
     }, seconds * 1000);
 });
 
-getPromise(1)
+getPromise(2)
     .then((res) => {
-        console.log('----1----');
+        console.log('----1 then----');
+        console.log(res);
+
+        return getPromise(2);
+    }).catch((res) => {
+        console.log('----1 catch----')
+        console.log(res);
+
+        return getPromise(2);
+    }).then((res) => {
+        console.log('----2 then----');
         console.log(res);
 
         return getPromise(1);
     }).catch((res) => {
-        console.log('----1 catch----')
+        console.log('----2 catch----')
         console.log(res);
-    })
+
+        return getPromise(2)
+    }).then((res) => {
+        console.log('----3 then----');
+        console.log(res);
+    }).catch((res) => {
+        console.log('----3 catch----')
+        console.log(res);
+    }
+    );
+
+Promise.all([
+    getPromise(2),
+    getPromise(4),
+    getPromise(2),
+]).then((res) => {
+    console.log('----all then----');
+    console.log(res);
+}).catch((res) => {
+    console.log('----all catch----')
+    console.log(res);
+});
+
+//결과도 list로 반환됨.
+//모두 then일 때 모두 list로 결과 반환
+//하나라도 catch로 빠지면 그 즉시 중단됨
