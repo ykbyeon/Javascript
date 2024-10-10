974ccdcc7daf307cc0716e1b4f10e95319ad2320
YK. Byeon <ykbyeon@gmail.com>
2024-10-10 11:18:38 +0900

    async, await, promise 예제 2
    좀 더 깊은내용 설명 by 김광식 부장님

---
+++ CodeFactory/4_async_programming/99_async_await_promise_sample_2(By_ks.kim2).js
@@ -0,0 +1,29 @@
+const fnSync01 = () => {
+    return 1;
+}
+
+const fnASync01 = async () => {
+    console.log ( 'fnASync01');
+    return 2;
+}
+
+const fnSyncButPromise = () => {
+    const pr = new Promise( (resolve, reject) => {
+        console.log ( 'fnSyncButPromise');
+        resolve(3);
+    });
+    return pr;
+}
+
+
+const a = await fnASync01(); // 어딘지 모를 위치에 홰당 모듈을 실행하도록 위임
+const b = await fnSyncButPromise(); // 어딘지 모를 위치에 홰당 모듈을 실행하도록 위임
+const c = fnSync01(); // blocking 후 처리 ...
+
+console.log ( a, b, c);
+
+console.log ( a );
+console.log(b);
+//a.then( (v) => { console.log ( `A Result : ${v}`)});    //  위임 처리후 결과를 출력해줘 ...
+//b.then( (v) => { console.log ( `B Result : ${v}`)});    //  위임 처리후 결과를 출력해줘 .. 
+console.log(`C Result : ${c}`); //   이미 처리됨 .. 
