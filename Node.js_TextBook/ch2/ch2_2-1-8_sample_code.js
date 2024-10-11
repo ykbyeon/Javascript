8481891635c7fb53ddbae25e029c06d113a03f46
YK. Byeon <ykbyeon@gmail.com>
2024-10-11 12:20:41 +0900

    설명 준비

--- Node.js_TextBook/ch2/ch2_2-1-8_sample_code.js
+++ Node.js_TextBook/ch2/ch2_2-1-8_sample_code.js
@@ -1,30 +1,3 @@
-async function findAndSaveUser(Users) {
-    try {
-        let user = await Users.findOne({});
-        user.name = 'zero';
-        user = await user.save();
-        user = await Users.findOne({ gender: 'm' });
-        //생략
-    }
-    catch (error) {
-        console.error(error);
-    }
-};
-
-const findAndSaveUser_arrow = async (Users) => {
-    try {
-        let user = await Users.findOne({});
-        user.name = 'zero';
-        user = await user.save();
-        user = await Users.findOne({ gender: 'm' });
-        //생략
-    }
-    catch (error) {
-        console.error(error);
-    }
-};
-
-
 const promise1 = Promise.resolve('Success1');
 const promise2 = Promise.resolve('Success2');
 
