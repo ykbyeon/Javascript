f6a7e35b331a482c6a04017b760b6f9ab1c3f6f3
YK.Byeon <ykbyeon@gmail.com>
2024-10-23 18:32:07 +0900

    생활코딩

--- OpenTutorials/web2_Node.js/main6_update.js
+++ OpenTutorials/web2_Node.js/main6_update.js
@@ -58,7 +58,17 @@ var app = http.createServer(function (request, response) {
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     var title = queryData.id;
                     var body = `<h2>${title}</h2><p>${description}</p>`;
-                    var control = `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`;
+                    var control = `<a href="/create">create</a>
+                                   <a href="/update?id=${title}">update</a>
+                                   <form action="/delete_process" method="post">
+                                        <input type = "hidden" name="id" value="${title}">
+                                        <input type = "submit" value="delete">
+                                   </form> `;
+                    // !!!! 주의 !!!!!F
+                    // delete는 Link방식으로 구현하면 절대안됨.
+                    // Link는 노출되기 때문에, Link만 알아서 호출하면
+                    // 데이터 삭제가 발생할 수도 있기 때문
+                    // 이번 강의에서 delete는 form으로 구현함
                     var template = templateHTML(title, list, body, control);
 
                     response.writeHead(200);
@@ -94,8 +104,7 @@ var app = http.createServer(function (request, response) {
             fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
                 response.writeHead(302, { location: `/?id=${title}` });
                 response.end();
-            }
-            )
+            });
         });
     } else if (pathname === '/update') {
         fs.readdir('./data', (error, filelist) => {
@@ -115,6 +124,36 @@ var app = http.createServer(function (request, response) {
 
                 response.writeHead(200);
                 response.end(template);
+            });
+        });
+    } else if (pathname === '/update_process') {
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', () => {
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, (error) => {
+                fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
+                    response.writeHead(302, { location: `/?id=${title}` });
+                    response.end();
+                });
+            });
+        });
+    } else if (pathname === '/delete_process') {
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', () => {
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, (error) => {
+                response.writeHead(302, { location: `/` });
+                response.end();
             });
         });
     } else {
