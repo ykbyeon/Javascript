32f36feafbe340adf528b5da4f45f7da306fba75
YK.Byeon <ykbyeon@gmail.com>
2024-10-28 15:49:55 +0900

    실습

--- Node.js_TextBook/ch3/3-6-4_threadpool.js
+++ Node.js_TextBook/ch3/3-6-4_threadpool.js
@@ -4,3 +4,11 @@ const pass = 'pass';
 const salt = 'salt';
 const start = Date.now();
 
+const thread_count = 10
+
+for (let i = 0; i < thread_count; i++) {
+    crypto.pbkdf2(pass, salt, 1000000, 128, 'sha512', (err, key) => {
+        console.log(`${i}: `, Date.now() - start, key);
+    })
+}
+
