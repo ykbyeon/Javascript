94d604d1f310d81d03766709ba7fad7c4ddde209
YK.Byeon <ykbyeon@gmail.com>
2024-11-01 14:44:05 +0900

    http서버 연습

--- Node.js_TextBook/ch4/4.3_Cookie_Session/session.js
+++ Node.js_TextBook/ch4/4.3_Cookie_Session/session.js
@@ -11,6 +11,8 @@ const parseCookies = (cookie = '') =>
             return acc;
         }, {});
 
+const session = {};
+
 http.createServer(async (req, res) => {
     const cookies = parseCookies(req.headers.cookie);
 
@@ -21,14 +23,21 @@ http.createServer(async (req, res) => {
         const expires = new Date();
         //쿠키 유효시간을 현재 시간 + 5분으로 설정
         expires.setMinutes(expires.getMinutes() + 5);
+
+        const uniqueInt = Date.now();
+        session[uniqueInt] = {
+            name,
+            expires
+        };
+
         res.writeHead(302, {
             Location: '/',
-            'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            'Set-Cookie': `session=${uniqueInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
         });
         res.end();
-    } else if (req.url === '/' && cookies.name !== undefined) {    //주소가 /이면서 name이라는 쿠키가 있는 경우
+    } else if (cookies.session && session[cookies.session] !== undefined ? session[cookies.session].expires : new Date() - 1 > new Date()) {    //세션 쿠키가 존재하고, 만료 기간이 지나지 않았다면
         res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-        res.end(`${cookies.name}님 안녕하세요.`);
+        res.end(`${session[cookies.session].name}님 안녕하세요.`);
     } else {    //주소가 /이면서 name이라는 쿠키가 없는 경우
         try {
             const data = await fs.readFile(path.join(__dirname, 'cookie2.html'));
@@ -41,6 +50,6 @@ http.createServer(async (req, res) => {
         }
     }
 })
-    .listen(8084, () => {
-        console.log('8084번 포트에서 서버 대기 중입니다.!');
+    .listen(8085, () => {
+        console.log('8085번 포트에서 서버 대기 중입니다.!');
     });
