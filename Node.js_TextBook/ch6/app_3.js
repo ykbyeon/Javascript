be894c07cd8004f0fe7bd8c767858c9f53037e31
YK.Byeon <ykbyeon@gmail.com>
2024-11-04 13:05:51 +0900

    ch6 express서버 실습

--- Node.js_TextBook/ch6/app_3.js
+++ Node.js_TextBook/ch6/app_3.js
@@ -1,31 +1,8 @@
 const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const dotenv = require('dotenv');
 const path = require('path');
 
-dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
-
-app.use(morgan(process.argv[2] || 'dev'));
-app.use('/', express.static(path.join(__dirname, 'public')));
-//app.use('/byc', express.static(path.join(__dirname, 'ykbyeon')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    },
-    name: 'session-cookie',
-}));
-
 
 app.use((req, res, next) => {
     console.log('모든 요청에 다 실행됩니다.');
