06842ef28892aa12f91f7576afa565b761d05653
YK.Byeon <ykbyeon@gmail.com>
2024-11-04 16:37:30 +0900

    ch6 express 서버 실습

--- Node.js_TextBook/ch6/app.js
+++ Node.js_TextBook/ch6/app.js
@@ -27,6 +27,39 @@ app.use(session({
 }));
 
 
+const multer = require('multer');
+const fs = require('fs');
+
+try {
+    fs.readdirSync('uploads');
+} catch (err) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.')
+    fs.mkdirSync('uploads');
+}
+
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/');
+        },
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { filesize: 5 * 1024 * 1024 },
+});
+
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'multipart.html'));
+});
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log(req.file);
+    res.send('ok');
+});
+
 app.use((req, res, next) => {
     console.log('모든 요청에 다 실행됩니다.');
     next();
