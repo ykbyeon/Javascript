b99df97f329a79cf587893588c79bbf5475eb724
YK. Byeon <ykbyeon@gmail.com>
2024-11-06 01:21:09 +0900

    실습코드

---
+++ Node.js_TextBook/ch6/app_5.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+const app = express();
+//app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
+
+app.use(morgan(process.argv[2] || 'dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+//app.use('/byc', express.static(path.join(__dirname, 'ykbyeon')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
+
+
+const multer = require('multer');
+const fs = require('fs');
+
+try {
+    fs.readdirSync('uploads');
+} catch (err) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.')
+    fs.mkdirSync('uploads');
+}
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/');
+        },
+        filename(req, file, done) {
+            file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8');
+            //출처: https://nami-socket.tistory.com/51 
+
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { filesize: 5 * 1024 * 1024 },
+});
+
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'multipart.html'));
+});
+
+app.get('/upload2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'multipart2.html'));
+});
+
+app.get('/upload3', (req, res) => {
+    res.sendFile(path.join(__dirname, 'multipart3.html'));
+});
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log(req.file, req.body);
+    res.send('ok');
+});
+
+app.post('/upload2', upload.array('many'), (req, res) => {
+    console.log(req.files, req.body);
+    res.send('ok');
+});
+
+app.post('/upload3',
+    upload.fields([{ name: 'image1' }, { name: 'image2' }]),
+    (req, res) => {
+        console.log(req.files, '||||||||||||||||||||', req.body);
+        res.send('ok');
+    });
+
+
+const indexRouter = require('./routes/');
+const userRouter = require('./routes/user');
+
+
+
+
+app.use((req, res, next) => {
+    console.log('모든 요청에 다 실행됩니다.');
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    console.log('GET / 요청에서만 실행됩니다.');
+    res.locals.test = 'Data Test';
+    next();
+}, (req, res, next) => {
+    console.log(req.url);
+    console.log(res.locals.test);
+    //res.send('YK.Byeon 응답입니다.'); // https://www.perplexity.ai/search/const-express-require-express-m2yAeCWJRlK9KMwqUXE4PA#1
+    next();
+}, (req, res, next) => {
+    //throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
+    console.log('에러는 에러 처리 미들웨어로 갑니다.');
+    next();
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
+
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+    //next();
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
