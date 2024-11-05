bfc8bb1b6be24ed465ffd6025f2ad49373c947d9
YK.Byeon <ykbyeon@gmail.com>
2024-11-05 13:31:37 +0900

    ch6 express 서버

--- Node.js_TextBook/ch6/routes/user.js
+++ Node.js_TextBook/ch6/routes/user.js
@@ -9,4 +9,19 @@ router.get('/', (req, res, next) => {
     console.log('user 라우터 입니다.')
 });
 
+
+//http://localhost:8080/user/test/like22?name=yk.byeon&age=47&address=seoul
+router.get('/test/:id', (req, res) => {
+    console.log(req.params, req.query, req.body);
+    res.send({ "param": req.params, "query": req.query, 'body': req.body });
+    console.log('여기만 실행됩니다.');
+});
+
+router.get('/test/like', (req, res) => {
+    console.log('여기는 실행되지 않습니다.');
+});
+
+
+
+
 module.exports = router;
