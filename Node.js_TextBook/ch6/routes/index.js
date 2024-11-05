da0a1e0fdb2bb79f87b6b923f564f4afcd274d28
YK.Byeon <ykbyeon@gmail.com>
2024-11-05 13:55:48 +0900

    ch6.3 Router실습

--- Node.js_TextBook/ch6/routes/index.js
+++ Node.js_TextBook/ch6/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const path = require('path');
+
 // GET / 라우터
 router.get('/', (req, res, next) => {
     console.log('다음 router로 이동합니다.')
@@ -20,6 +22,16 @@ router.get('/', (req, res, next) => {
 }, (req, res, next) => {
     console.log('index 라우터 입니다.')
 });
+
+
+router.route('/abc')
+    .get((req, res) => {
+        //res.send('GET /abc');
+        res.sendFile(path.join(__dirname, '..', 'multipart4.html'))
+    })
+    .post((req, res) => {
+        res.send('POST /abc');
+    });
 
 module.exports = router;
 
