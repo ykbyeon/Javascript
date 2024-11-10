c8398a405945c6ec1a739a2fb76d8e03cd35a9ce
YK. Byeon <ykbyeon@gmail.com>
2024-11-11 00:52:09 +0900

    main_test_client 실습

--- main_test_client/routes/samples/index2.js
+++ main_test_client/routes/samples/index2.js
@@ -6,6 +6,8 @@ const router = asyncify(express.Router());
 import path from "path";
 import sampleCtrl from "#controllers/sampleController.js";
 
+import commonJS from "#public/js/common.js";
+
 
 router.get("/", async (req, res) => {
     const data = {
@@ -17,10 +19,37 @@ router.get("/", async (req, res) => {
             userRole: res.locals.user.userRole
         },
 
-        'data': {},
+        'remote_data': [], // 2024.11.07 sample.ejs파일내 처리를 위해 추가 yk.byeon
     };
     res.render("samples/sample2", data );
 });
+
+router.get("/remote_list", async (req, res) => {
+    const url = "http://localhost:3000/samples/list";
+
+    console.log(req.query.id, req.query.title )
+    const params = {
+        ID: req.query.id,
+        TITLE: req.query.title
+      };
+
+    const result = await commonJS.sendPostAjax(url, params);
+
+    const data = {
+        "title": "제목 테스트 입니다.",
+        "message": "My World!!!",
+        'infos': {
+            mainTitle: 'Sample',
+            userID: res.locals.user.userID,
+            userRole: res.locals.user.userRole
+        },
+
+        'remote_data': result, // 2024.11.07 sample.ejs파일내 처리를 위해 추가 yk.byeon
+    };
+
+    res.render("samples/sample2", data );
+});
+
 
 router.post("/list", async (req, res) => {
     let params = req.body;
